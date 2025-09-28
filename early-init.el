eceae65863998b99398bafa9542c66e4e084085d
Vegard Seines <vegsei@gmail.com>
2025-09-28 20:51:49 +0200

    Update

--- early-init.el
+++ early-init.el
@@ -1,3 +1,8 @@
+(setq package-enable-at-startup nil)
+(setq package-quickstart nil)
+(setq straight-check-for-modifications nil)
+(fmakunbound 'package-initialize)
+
 (defvar bootstrap-version)
 (let ((bootstrap-file
        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
@@ -13,5 +18,15 @@
 
 (straight-use-package 'use-package)
 (setq straight-use-package-by-default t)
-(setq package-enable-at-startup nil)
-(setq package-quickstart nil)
+
+(defun my/org-auto-tangle-on-save ()
+  "Automatically tangle Org file on save if it has code blocks."
+  (when (and (string-equal (buffer-file-name)
+                           (expand-file-name "~/.emacs.d/README.org"))
+             (derived-mode-p 'org-mode))
+    (org-babel-tangle)))
+
+(add-hook 'org-mode-hook
+          (lambda ()
+            (add-hook 'after-save-hook #'my/org-auto-tangle-on-save
+		      nil 'make-it-local)))
